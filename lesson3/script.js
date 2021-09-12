97e317cfc97ead2bc47fbb633c9bbd44c904274b
TuckerLandis <landistuckerc@gmail.com>
2021-09-12 17:01:01 -0500

    scene, geo, material, sizes, camera

--- lesson3/script.js
+++ lesson3/script.js
@@ -1,2 +1,20 @@
-console.log('Hello Three.js');
-console.log(THREE);
+// scene
+const scene = new THREE.Scene()
+
+// red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add( mesh )
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera -- 2 params - fov, aspect ratio
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+scene.add(camera)
+
+// Renderer
